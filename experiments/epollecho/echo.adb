c7b29868f1d5a3511cb0e7685ffc4746eaadecab
R. Tyler Croy <tyler@monkeypox.org>
2011-02-19 13:42:08 -0800

    Separate the accept block in the echo callback

--- experiments/epollecho/echo.adb
+++ experiments/epollecho/echo.adb
@@ -8,12 +8,21 @@ use Ada.Text_IO,
 package body Echo is
     procedure Call_Me_Back (Sock : Socket_Type;
                             Ctx : Echo_State) is
-        New_Sock : Socket_Type;
         Addr : Sock_Addr_Type := Ctx.Server_Addr;
     begin
         Put_Line (">>> Starting Call_Me_Back");
 
-        Accept_Socket(Sock, New_Sock, Addr);
+        if Sock = Ctx.Listening_Socket then
+            Put_Line (">>> Accept_Socket");
+            declare
+                New_Sock : Socket_Type;
+            begin
+                Accept_Socket(Sock, New_Sock, Addr);
+            end;
+            return;
+        end if;
+
+        Put_Line (">>> Ready to read data");
     end Call_Me_Back;
 end Echo;
 
